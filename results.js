12df910d043a8b306e6a97c26ce3d417fd95857d
Jan Domanski <jandom@gmail.com>
2018-08-12 12:49:53 +0100

    Working heatmap with results

---
+++ results.js
@@ -0,0 +1,61 @@
+var results = [
+  {
+    "Name": "Workshop Coffee",
+    "Price": 3.2,
+    "Lat": 51.525155,
+    "Lng": -0.0879743
+  },
+  { "Name": "Dolcezza", "Price": 2.4, "Lat": 51.5225036, "Lng": -0.0851814 },
+  { "Name": "Hoxton", "Price": 2.8, "Lat": 51.5250183, "Lng": -0.0840554 },
+  {
+    "Name": "Shoreditch Grind",
+    "Price": 3.0,
+    "Lat": 51.5259274,
+    "Lng": -0.0881589
+  },
+  { "Name": "Starbucks", "Price": 2.65, "Lat": 51.5263637, "Lng": -0.0887776 },
+  { "Name": "Abokado", "Price": 2.49, "Lat": 51.5260926, "Lng": -0.0884314 },
+  { "Name": "Pret", "Price": 2.45, "Lat": 51.5241008, "Lng": -0.0896296 },
+  { "Name": "Attendant", "Price": 2.9, "Lat": 51.5256691, "Lng": -0.0847031 },
+  { "Name": "Fix 126", "Price": 2.75, "Lat": 51.526123, "Lng": -0.082403 },
+  {
+    "Name": "Ozone Coffee Roasters",
+    "Price": 0.0,
+    "Lat": 51.5246824,
+    "Lng": -0.0890492
+  },
+  { "Name": "Trade", "Price": 2.8, "Lat": 51.5175043, "Lng": -0.0760209 },
+  { "Name": "Fazenda", "Price": 2.5, "Lat": 51.5169728, "Lng": -0.0784918 },
+  {
+    "Name": "Allpress Espresso Bar",
+    "Price": 0.0,
+    "Lat": 51.5241762,
+    "Lng": -0.0746331
+  },
+  {
+    "Name": "Barber and Parlour",
+    "Price": 0.0,
+    "Lat": 51.5244072,
+    "Lng": -0.0761465
+  },
+  {
+    "Name": "Leila's Shop",
+    "Price": 0.0,
+    "Lat": 51.5259796,
+    "Lng": -0.0757313
+  },
+  { "Name": "Cafe Z", "Price": 0.0, "Lat": 51.5239379, "Lng": -0.0842921 },
+  {
+    "Name": "Top Daily caffe",
+    "Price": 0.0,
+    "Lat": 51.5237657,
+    "Lng": -0.084244
+  },
+  {
+    "Name": "CLIFTONS CAFE",
+    "Price": 0.0,
+    "Lat": 51.5235262,
+    "Lng": -0.0837954
+  }
+]
+;
